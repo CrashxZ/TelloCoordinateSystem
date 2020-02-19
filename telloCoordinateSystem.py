87949cef97bfbb53f523a055be896dc66f7b1284
Mainak Mondal <mainakme2140@gmail.com>
2020-02-19 14:42:08 +0700

    error fix

--- telloCoordinateSystem.py
+++ telloCoordinateSystem.py
@@ -5,49 +5,46 @@ ENU = 1
 # Unit = Meters
 # Delta Trajectory
 
+
+
 def moveENU(x,y,z):
 	if x>0:
-		tello.east(x)
+		tello.right(x)
 	elif x<0:
-		tello.west(x)
+		tello.left(x*-1)
 
 	if y>0:
 		tello.forward(y)
 	elif y<0:
-		tello.back(y)
+		tello.back(y*-1)
 
 	if z>0:
 		tello.up(z)
 	elif z<0:
-		tello.down(z)
+		tello.down(z*-1)
 
 def moveNED(x,y,z):
 	if y>0:
-		tello.east(y)
+		tello.right(y)
 	elif y<0:
-		tello.west(y)
+		tello.left(y*-1)
 
 	if x>0:
 		tello.forward(x)
 	elif x<0:
-		tello.back(x)
+		tello.back(x*-1)
 
 	if z>0:
 		tello.down(z)
 	elif z<0:
-		tello.up(z)
+		tello.up(z*-1)
 
 
-def move(x,y,y):
-	x=x*100
-	y=y*100
-	z=z*100
-	if(NED==1):
+def move(x,y,z):
+	x=int(x*100)
+	y=int(y*100)
+	z=int(z*100)
+	if(ENU==0):
 		moveNED(x,y,z)
-	else 
+	else: 
 		moveENU(x,y,z)
-
-
-
-
-
