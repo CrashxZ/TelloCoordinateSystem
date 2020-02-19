408f328da1e87757afb463559dae08844893147d
Mainak Mondal <mainakme2140@gmail.com>
2020-02-19 15:02:14 +0700

    fix

--- telloCoordinateSystem.py
+++ telloCoordinateSystem.py
@@ -1,50 +1,48 @@
-from easytello import tello 
-tello = tello.Tello()
+class telloC:
+	def moveENU(self,x,y,z):
+		if x>0:
+			self.tello.right(x)
+		elif x<0:
+			self.tello.left(x*-1)
 
-ENU = 1
-# Unit = Meters
-# Delta Trajectory
+		if y>0:
+			self.tello.forward(y)
+		elif y<0:
+			self.tello.back(y*-1)
 
+		if z>0:
+			self.tello.up(z)
+		elif z<0:
+			self.tello.down(z*-1)
 
+	def moveNED(self,x,y,z):
+		if y>0:
+			self.tello.right(y)
+		elif y<0:
+			self.tello.left(y*-1)
 
-def moveENU(x,y,z):
-	if x>0:
-		tello.right(x)
-	elif x<0:
-		tello.left(x*-1)
+		if x>0:
+			self.tello.forward(x)
+		elif x<0:
+			self.tello.back(x*-1)
 
-	if y>0:
-		tello.forward(y)
-	elif y<0:
-		tello.back(y*-1)
-
-	if z>0:
-		tello.up(z)
-	elif z<0:
-		tello.down(z*-1)
-
-def moveNED(x,y,z):
-	if y>0:
-		tello.right(y)
-	elif y<0:
-		tello.left(y*-1)
-
-	if x>0:
-		tello.forward(x)
-	elif x<0:
-		tello.back(x*-1)
-
-	if z>0:
-		tello.down(z)
-	elif z<0:
-		tello.up(z*-1)
+		if z>0:
+			self.tello.down(z)
+		elif z<0:
+			self.tello.up(z*-1)
 
 
-def move(x,y,z):
-	x=int(x*100)
-	y=int(y*100)
-	z=int(z*100)
-	if(ENU==0):
-		moveNED(x,y,z)
-	else: 
-		moveENU(x,y,z)
+	def move(self,x,y,z):
+		ENU = 1
+		x=int(x*100)
+		y=int(y*100)
+		z=int(z*100)
+		if(ENU==0):
+			self.moveNED(x,y,z)
+		else: 
+			self.moveENU(x,y,z)
+
+	def __init__(self, driver):
+        	self.tello = driver
+		# Unit = Meters
+		# Delta Trajectory
